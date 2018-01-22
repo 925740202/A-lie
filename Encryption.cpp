1e01c892d0d45555e7e8fdca3644d6548a7e7fea
A-lie <925740202@qq.com>
2018-01-22 22:35:15 +0800

    1.22
    浪费一整天在vs2015

---
+++ Encryption.cpp
@@ -0,0 +1,41 @@
+/*Encryption Algorithm
+made by Math_Alie
+2018-1-22*/
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	char s,ns;
+	int k;
+	cout<<"Please input your word that wants to encryption:"<<endl;
+	cin>>s;
+	cout<<"Please input the k:"<<endl;
+	cin>>k;
+	if(s<='Z')
+	   ns=((s-'A')+k)%26+'A';
+    else
+	   ns=((s-'a')+k)%26+'a';
+    cout<<"After encryption:"<<ns<<endl;
+    while(1)
+    {
+    cout<<"Do you want to continue? Y/N"<<endl;
+    char answer;
+    cin>>answer;
+    if(answer=='Y')
+    {
+    cout<<"Please input your word that wants to encryption:"<<endl;
+	cin>>s;
+	cout<<"Please input the k:"<<endl;
+	cin>>k;
+    if(s<='Z')
+	   ns=((s-'A')+k)%26+'A';
+    else
+	   ns=((s-'a')+k)%26+'a';
+    cout<<"After encryption:"<<ns<<endl;
+	}
+	else break;
+    }
+	return 0;
+}
+
